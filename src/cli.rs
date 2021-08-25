f1106f8f82af6a44a1f6a750f205ad920789af81
joakin <joaquin@chimeces.com>
2021-08-25 21:24:19 +0200

    Add a readline implementation for a better REPL editing experience

--- src/cli.rs
+++ src/cli.rs
@@ -2,9 +2,9 @@ use crate::ast;
 use crate::compiler;
 use crate::source::Source;
 use crate::type_env::TypeEnv;
+use rustyline::{error::ReadlineError, Editor};
 use std::collections::HashMap;
 use std::fs;
-use std::io::{self, Write};
 use std::process;
 use std::rc::Rc;
 
@@ -23,25 +23,40 @@ pub fn prompt() {
     };
     let mut module_interfaces: HashMap<String, Rc<TypeEnv>> = HashMap::new();
 
+    let mut rl = Editor::<()>::new();
+    // TODO: figure out how to store this inthe global config dir
+    // let _ = rl.load_history(repl_history_path);
+
     loop {
-        print!("> ");
-        io::stdout().flush().expect("Flushing REPL prompt failed");
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap_or_else(|err| {
-            println!("Failed to read line");
-            println!("  {}", err);
-            process::exit(1);
-        });
-        println!("");
-
-        let file = Source::new_orphan(&input);
-        match compiler::compile_repl_entry(&mut module, &mut module_interfaces, &file) {
-            Ok(out) => println!("{}", out),
-            Err(errs) => println!("{}", errs),
-        };
-        println!("");
+                let file = Source::new_orphan(&line);
+                match compiler::compile_repl_entry(&mut module, &mut module_interfaces, &file) {
+                    Ok(out) => println!("{}", out),
+                    Err(errs) => println!("{}", errs),
+                };
+                println!("");
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Bye!");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Bye!");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
     }
+
+    // TODO: See above on rl.load_history
+    // rl.append_history(repl_history_path).unwrap();
 }
 
 fn source_from_path(file_path: &str) -> String {
